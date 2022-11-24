bf833b0151bf4eba4e7613da667b97d42ffb2a5f
fsffaeswf <114169974+fsffaeswf@users.noreply.github.com>
2022-11-24 21:53:15 +0300

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,15 +1,4 @@
 #pragma once
-bool isprime(int n) {
-	int i = 2, k = 0;
-	while (n / 2 >= i) {
-		if (n % i == 0) {
-			k++;
-		}
-		i++;
-	}
-	bool m = k == 0;
-	return m;
-}
 int digitN(int K, int N) {
 	int n = 0, k1{};
 	while (K>0) {

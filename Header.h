d292812ce1236cdba5682065c0314f08d5f437e5
fsffaeswf <114169974+fsffaeswf@users.noreply.github.com>
2022-11-24 18:34:03 +0300

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+//25
+int digitCount(int k) {
+	int n = 0;
+	while (k > 0) {
+		n++;
+		k = k / 10;
+	}
+	return n;
+}
+//30 решить потом
+int digitN(int K, int N) {
+	int n = 0,k1;
+	while (n == k1) {
+		k1 = K % 10;
+		K = K / 10;
+		n++;
+	}
+	if (N > n) {
+		return -1;
+	}
+}
+
---
+++ Source.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "Header.h"
+#include <ctime>
+using namespace std;
+int main() {
+	const int N = 5;
+	int arr[N], k = 0;
+	srand(time(nullptr));
+	for (int i = 0; i < 10; i++) {
+		arr[i] = rand() % 1000;
+		cout << arr[i] << ' ';
+	}
+	cout << endl;
+	/*29
+	for (int i = 0; i < 10; i++) {
+		cout << i << ":" << digitCount(arr[i]) << ' ';
+	}*/
+}
